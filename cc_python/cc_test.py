1c7fb77cd580a4b085d395af875d8f26b1fa7a1f
adam-lafontaine <adam.lafontaine@icloud.com>
2019-02-07 18:21:49 -0500

    working test file

--- cc_python/cc_test.py
+++ cc_python/cc_test.py
@@ -4,30 +4,46 @@ def result_msg(expected, result):
     return f"{'Success' if result == expected else 'Fail'}"
 
 def test_has_bit():
-    print("/nTest: has_bit(binary)")
+    print("\nTest: has_bit(binary)")
+
     source = [{'in': '01001', 'out': True}, {'in': '1010', 'out': True}, {'in': '0000000', 'out': False}]
+    success = "Pass"
+
     for src in source:
         src_in = src['in']
         src_out = src['out']
         result = cc_lib.has_bit(src_in)
+        if result != src_out:
+            success = "Fail"
+
         print(f"   input = {src_in}")
         print(f"expected = {src_out}")
         print(f"  result = {result}")
         print(result_msg(src_out, result))
+
+    return success
 
 #---------------------------------------
 
 def test_flip_bit():
-    print("/nTest: has_bit(binary)")
+    print("\nTest: flip_bit(binary)")
+
     source = [{'in': '0', 'out': '1'}, {'in': '1', 'out': '0'}]
+    success = "Pass"
+
     for src in source:
         src_in = src['in']
         src_out = src['out']
-        result = cc_lib.has_bit(src_in)
+        result = cc_lib.flip_bit(src_in)
+        if result != src_out:
+            success = "Fail"
+
         print(f"   input = {src_in}")
         print(f"expected = {src_out}")
         print(f"  result = {result}")
         print(result_msg(src_out, result))
+
+    return success
 
 #-------------------------------------------
 
@@ -35,8 +51,20 @@ def test_flip_bit():
 def main():
     print("=== Cut Calculator Test ===")
 
-    test_has_bit()
-    test_flip_bit()
+    tests = {
+        'has_bit()': test_has_bit(),
+        'flip_bit()' : test_flip_bit()
+    }
+
+    spaces = 0
+    for key in tests:
+        spaces = len(key) if len(key) > spaces else spaces
+
+    print("\nSummary:")
+    for key in tests:
+        print(f"{key.rjust(spaces)}: {tests[key]}")
+
+    print("\nDone.")
 
 #==================================
 if __name__ == '__main__':
