dc8cb7679bbd997e7c72beab023c05a89ccf3629
adam-lafontaine <adam.lafontaine@icloud.com>
2019-10-14 13:40:22 -0400

    remove old tests

--- c_cc/cc_test.c
+++ c_cc/cc_test.c
@@ -10,30 +10,14 @@ void print(const char* msg) {
 // gcc -o run_tests cc_test.c sb_stringbuilder.c
 
 int main(int argc, char **argv) {
-    char buffer[100];
-
-    test_stringbuilder_create_destroy(buffer, 100);
-    print(buffer);
+    
     
 }
 
 //===========================
 
 
-void test_stringbuilder_create_destroy(char* buffer, size_t max_size) {
 
-    if(max_size < 50) {
-        return;
-    }
-
-    size_t size = 10;
-
-    string_t* str = sb_create_empty(size);
-    
-    sb_destroy(str);        
-
-    sprintf(buffer, "Pass");
-}
 
 
 
