3a769f2811532044589b84f68c08e541bb2abefb
adam-lafontaine <adam.lafontaine@gmail.com>
2023-08-04 12:08:36 -0400

    result tests

--- cpp_cc/test_ops_main.cpp
+++ cpp_cc/test_ops_main.cpp
@@ -8,7 +8,6 @@ namespace cc = cut_calculator;
 
 static void print(std::vector<f32> const& items, std::vector<f32> const& containers, cc::SortResult const& result)
 {
-	printf("\n");
 	printf("     items:");
 	for (auto n : items)
 	{
@@ -102,15 +101,121 @@ static bool test_container_ids(std::vector<f32> const& containers, cc::SortResul
 }
 
 
+static bool test_container_capacities(std::vector<f32> const& containers, cc::SortResult const& sort_result)
+{
+	for (auto const& c : sort_result.sorted)
+	{
+		if (c.container_capacity != containers[c.container_id])
+		{
+			return false;
+		}
+
+		return true;
+	}
+}
+
+
+static bool test_item_ids(std::vector<f32> const& items, cc::SortResult const& sort_result)
+{
+	std::vector<u32> ids;
+
+	for (auto const& c : sort_result.sorted)
+	{
+		for (auto i : c.item_ids)
+		{
+			ids.push_back(i);
+		}		
+	}
+
+	for (auto i : sort_result.unsorted_item_ids)
+	{
+		ids.push_back(i);
+	}
+
+	if (ids.size() != items.size())
+	{
+		return false;
+	}
+
+	std::sort(ids.begin(), ids.end());
+
+	for (u32 i = 0; i < (u32)ids.size(); ++i)
+	{
+		if (ids[i] != i)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+static bool test_item_sizes(std::vector<f32> const& items, cc::SortResult const& sort_result)
+{
+	for (auto const& c : sort_result.sorted)
+	{
+		f32 size = 0.0f;
+		for (auto i : c.item_ids)
+		{
+			size += items[i];
+		}
+
+		if (c.item_size != size)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+static bool test_capacity_size(cc::SortResult const& sort_result)
+{
+	for (auto const& c : sort_result.sorted)
+	{
+		if (c.container_capacity < c.item_size)
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+
+static bool test_unsorted(std::vector<f32> const& items, std::vector<f32> const& containers, cc::SortResult const& sort_result)
+{
+	for (auto c : sort_result.unsorted_container_ids)
+	{
+		for (auto i : sort_result.unsorted_item_ids)
+		{
+			if (containers[c] >= items[i])
+			{
+				return false;
+			}
+		}
+	}
+
+	return true;
+}
+
+
 static void test_sort_results(std::vector<f32> const& items, std::vector<f32> const& containers, cc::SortResult const& sort_result)
 {
 	auto result = true;
 
 	result &= test_container_ids(containers, sort_result);
+	result &= test_container_capacities(containers, sort_result);
+	result &= test_item_ids(items, sort_result);
+	result &= test_item_sizes(items, sort_result);
+	result &= test_capacity_size(sort_result);
+	result &= test_unsorted(items, containers, sort_result);
 
 	if (result)
 	{
-		printf("sort_result: OK\n");
+		printf("\nsort_result: OK\n");
 	}
 }
 
@@ -132,8 +237,9 @@ static void test_easy()
 	
 	auto result = cc::sort(item_sizes, container_capacities);
 
-	print(item_sizes, container_capacities, result);
 	test_sort_results(item_sizes, container_capacities, result);
+	print(item_sizes, container_capacities, result);
+	
 }
 
 int main()
